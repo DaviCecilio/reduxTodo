421c3b18d6b01dc312379bfa147cf9c820a80832
Davi Cecílio <davim.cecilio@gmail.com>
2020-06-28 00:26:54 -0300

    Updates

---
+++ precache-manifest.e5ba5a937237ef7d2493a85c7cece11b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156ee5304692954266e9cf523c65e187",
+    "url": "/reduxTodo/index.html"
+  },
+  {
+    "revision": "5c7bb60d0728458d7a43",
+    "url": "/reduxTodo/static/js/2.0a6db241.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reduxTodo/static/js/2.0a6db241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a9757fed8df54c9e0df",
+    "url": "/reduxTodo/static/js/main.61d086dd.chunk.js"
+  },
+  {
+    "revision": "eed2e04cc28572f4888f",
+    "url": "/reduxTodo/static/js/runtime-main.23fd2f3e.js"
+  },
+  {
+    "revision": "4c680c66e71f694de3fa868fcf63bb2c",
+    "url": "/reduxTodo/static/media/githubIcon.4c680c66.svg"
+  },
+  {
+    "revision": "4b9d67074bdea08052fb1146e4779e9d",
+    "url": "/reduxTodo/static/media/linkedinIcon.4b9d6707.svg"
+  },
+  {
+    "revision": "9197c0b43a231f586662f6fc19f47763",
+    "url": "/reduxTodo/static/media/mailIcon.9197c0b4.svg"
+  },
+  {
+    "revision": "3548f67548ed9502d24acc4adf62577c",
+    "url": "/reduxTodo/static/media/personIcon.3548f675.svg"
+  }
+]);
